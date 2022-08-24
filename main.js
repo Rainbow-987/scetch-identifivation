f452a618b32e245354724ed4b421d4db74ed1178
Rainbow-987 <97032904+Rainbow-987@users.noreply.github.com>
2022-08-24 18:51:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function preload ()
+{
+    
+}
+
+
+function setup()
+{
+ canvas=createCanvas(300,300);
+    background("white");
+   // canvas.center();
+
+   canvas.position(630,300);
+}
+
+
+
+
+function draw()
+{
+    
+}
